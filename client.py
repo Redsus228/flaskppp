7c23f92d3c8d9d4243cca8b20d17d25a919aa49e
redsus <redoko228@mail.ru>
2026-09-10 19:58:57 +0700

    Добавлено склейка изображений и настроены стили к HTML коду, плюс переделана капча на галочку. Проект запускается спокойно через докер-компоуз.

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import os
 from io import BytesIO
 import base64
 img_data = None
-# создаем путь к файлу (для кросс-платформенности, например)
+# создаем путь к файлу
 path = os.path.join('./static','image0008.png')
 # читаем файл и енкодируем его в строку base64
 with open(path, 'rb') as fh:
